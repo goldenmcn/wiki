9ab3d7cd83dbf55708d2500de142a171c5b80fa5
HarvelsX <90945793+HarvelsX@users.noreply.github.com>
2023-06-05 23:53:00 +0300

    Fix socialcard & description in meta;

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -61,7 +61,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/zoloto-social-card.jpg',
+      image: 'img/zoloto-social-card.png',
       navbar: {
         title: 'Zoloto Wiki',
         logo: {
